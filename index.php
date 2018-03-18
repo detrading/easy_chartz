941970f410f4325e4d79b282c80a6fbd4d163ae0
nevn <37251801+nevn@users.noreply.github.com>
2018-03-18 10:29:22 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+//project requirements
+  // convert standard arrays(x & y) -> (x1, y1) or (x1, y1, y2) or  (x1, y1, y2, y3) and so on TODO be able to call with one function
+  //dygraphs and google charts
