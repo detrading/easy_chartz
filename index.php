fe24f78a053f396fb64a812abb4706239fa1b903
nevn <37251801+nevn@users.noreply.github.com>
2018-03-19 17:04:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,17 @@
 //project requirements
   // convert standard arrays(x & y) -> (x1, y1) or (x1, y1, y2) or  (x1, y1, y2, y3) and so on TODO be able to call with one function
   //dygraphs and google charts
+
+function dygraphCreate(){
+
+
+
+}
+
+
+//pass chart height, width, type requirements
+function googlechartCreate( ){
+
+
+
+}
